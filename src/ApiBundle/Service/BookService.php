4df98678737d49cdcca8b525f87543696dbfad00
Gabriel Borges <gabrielborgesdemoraes@gmail.com>
2019-01-16 17:52:15 -0200

    fix

--- src/ApiBundle/Service/BookService.php
+++ src/ApiBundle/Service/BookService.php
@@ -1,11 +1,24 @@
 <?php
 
-namespace AppBundle\Service;
+namespace ApiBundle\Service;
 
 use AppBundle\Service\BaseService;
-
-
+use Doctrine\ORM\EntityManager;
+//use AppBundle\Entity\Books;
 class DefaultController extends BaseService
 {
+    private $container;
 
+    public function __construct(EntityManager $em, $container){
+      $this->em = $em;
+      $this->container = $container;
+    }
+
+    /*
+    protected function getRepository(){
+      return $this->em->getRepository('AppBundle:Books');
+    }
+    */
+
+    
 }
